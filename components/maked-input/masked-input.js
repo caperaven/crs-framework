11c4633dd47259f4683e4be0211a7b5f87f37e70
caperaven <caperaven@gmail.com>
2022-09-20 23:27:06 +0200

    mask input initial

--- components/maked-input/masked-input.js
+++ components/maked-input/masked-input.js
@@ -10,16 +10,63 @@ export class MaskedInput extends HTMLInputElement {
 
 customElements.define("masked-input", MaskedInput, { extends: "input" });
 
+const maskValues = Object.freeze(["0", "#", "_"]);
+
 export class MaskManager {
     constructor(mask) {
         this._mask = mask;
         this.text = maskToText(mask);
+        this.values = this.text.split("");
+
+        this.setCursor(0);
+    }
+
+    /**
+     * set the cursor position
+     * @param index
+     */
+    setCursor(index) {
+        this._index = index;
+        this.stepCursor();
+    }
+
+    /**
+     * step the index until it finds a valid input
+     */
+    stepCursor() {
+        // we are at the end so exit
+        if (this._index == this._mask.length) return false;
+
+        // are we at a valid input point, if yes, exit
+        const maskAt = this._mask[this._index];
+        if (maskValues.indexOf(maskAt) != -1) return true;
+
+        // move cursor up
+        this._index += 1;
+        this.stepCursor();
+        return true;
+    }
+
+    set(char) {
+        if (this.stepCursor() == true && canEdit(this._mask[this._index], char)) {
+            this.values[this._index] = char;
+            this.text = this.values.join("");
+        }
     }
 }
 
-function maskToText(mask) {
+export function maskToText(mask) {
     return mask
         .split("0").join("_")
         .split("#").join("_")
+}
+
+export function canEdit(maskValue, char) {
+    const isAlpha = isNaN(char);
+
+    if (isAlpha == true && (maskValue == "#" || maskValue == "_")) return true;
+    if (isAlpha == false && (maskValue == "#" || maskValue == "0")) return true;
+
+    return false;
 }
 
--- test/components/masked-input/masked-input.test.js
+++ test/components/masked-input/masked-input.test.js
@@ -1,11 +1,40 @@
 import {initRequired} from "./../../mockups/init-required.js";
 
 import { assertEquals } from "https://deno.land/std@0.149.0/testing/asserts.ts";
-import { MaskManager } from "./../../../components/maked-input/masked-input.js";
+import { MaskManager, maskToText, canEdit } from "./../../../components/maked-input/masked-input.js";
 
 await initRequired();
+
+Deno.test("maskToText", async () => {
+    let text = maskToText("##-##-####");
+    console.assert(text, "__-__-____");
+})
+
+Deno.test("canEdit", async () => {
+    assertEquals(canEdit("0", "1"), true);
+    assertEquals(canEdit("0", "a"), false);
+    assertEquals(canEdit("#", "1"), true);
+    assertEquals(canEdit("#", "a"), true);
+    assertEquals(canEdit("_", "1"), false);
+    assertEquals(canEdit("_", "a"), true);
+})
 
 Deno.test("masked-input - set mask text", async () => {
     const manager = new MaskManager("(000) 00 000");
     assertEquals(manager.text, "(___) __ ___");
+    assertEquals(manager.values.length, 12);
+    assertEquals(manager._index, 1);
+})
+
+Deno.test("masked-input - set char", async () => {
+    const manager = new MaskManager("(000) 00 000");
+    assertEquals(manager.text, "(___) __ ___");
+    assertEquals(manager._index, 1);
+
+    manager.set("1");
+    assertEquals(manager.text, "(1__) __ ___");
+
+    manager.setCursor(0);
+    manager.set("2");
+    assertEquals(manager.text, "(2__) __ ___");
 })
