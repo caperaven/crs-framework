d82a0fe650a3ee517a3f66006906db48c4e671a1
Ashton Angel <ashton.angel@pragmaworld.net>
2022-11-07 09:46:23 +0200

    wip

--- packages/crs-process-api/action-systems/managers/dragdrop-manager/call.js
+++ packages/crs-process-api/action-systems/managers/dragdrop-manager/call.js
@@ -1,5 +1,1 @@
-export async function call(option, ...args) {
-    if (typeof option == "function") {
-        return await option(...args);
-    }
-}
+async function a(n,...t){if(typeof n=="function")return await n(...t)}export{a as call};
