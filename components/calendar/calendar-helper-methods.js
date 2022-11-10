c1139556dec949ebde24431363e44b680dbefa36
Charles Lemmert <81480730+Astro-Alien@users.noreply.github.com>
2022-11-10 12:32:13 +0200

    committing changes

--- components/calendar/calendar-helper-methods.js
+++ components/calendar/calendar-helper-methods.js
@@ -4,17 +4,17 @@ export class CalendarHelperMethods {
      * @param year
      * @returns {Promise<*[]>}
      */
-    static  async populateYears(year){
+    static  async populateYears(year) {
         let years = [];
         const startingYear =  (year - 100);
         const endingYear = (year + 100);
-        for( let i = startingYear; i <= endingYear ; i++){
+        for( let i = startingYear; i <= endingYear ; i++) {
             years.push(i);
         }
-        years.sort();
+
         return years;
     }
-    static async populateMonths(){
+    static async populateMonths() {
         return  ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
     }
 }
