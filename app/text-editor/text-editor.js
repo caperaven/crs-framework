d93c60ac1a0799a343bbdcc21b08929ec083a4f7
caperaven <caperaven@gmail.com>
2022-11-16 14:49:38 +0200

    add codemirror setup and component

--- app/text-editor/text-editor.js
+++ app/text-editor/text-editor.js
@@ -3,5 +3,7 @@ import "./../../components/text-editor/text-editor.js";
 export default class TextEditor extends crsbinding.classes.ViewBase {
     async connectedCallback() {
         await super.connectedCallback();
+        this.editor.value = "# Hello World";
+        console.log(this.editor.value);
     }
 }
