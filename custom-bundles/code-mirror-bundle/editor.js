303219288c9aeb7533d357f93ef080108d8fb9f8
caperaven <caperaven@gmail.com>
2022-11-17 13:34:26 +0200

    add json lang to code editor

--- custom-bundles/code-mirror-bundle/editor.js
+++ custom-bundles/code-mirror-bundle/editor.js
@@ -1,3 +1,4 @@
 export {EditorView, minimalSetup, basicSetup} from "codemirror";
 export * from "@codemirror/autocomplete";
 export * from "@codemirror/lang-markdown";
+export * from "@codemirror/lang-json";
