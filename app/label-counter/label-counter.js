95ef4703f73a2f4a0517c5590379880dd282695b
6Botter7 <7williamsandre7@gmail.com>
2022-11-21 15:28:56 +0200

    Label-Counter Component Added
    Label-Counter Component Added.
    
    label-counter converted to shadow DOM.
    
    Wrote test for label-counter (incomplete)

---
+++ app/label-counter/label-counter.js
@@ -0,0 +1,11 @@
+import "/components/label-counter/label-counter.js"
+
+export default class Welcome extends crsbinding.classes.ViewBase {
+    async connectedCallback() {
+        await super.connectedCallback();
+    }
+
+    async changed(event) {
+        this.setProperty("value", event.detail.value);
+    }
+}
---
+++ test/components/label-counter/label-counter.test.js
@@ -0,0 +1,38 @@
+import { beforeAll, beforeEach, describe, it} from "https://deno.land/std@0.157.0/testing/bdd.ts";
+import { assertEquals } from "https://deno.land/std@0.149.0/testing/asserts.ts";
+import {init} from "./../../../test/mockups/init.js";
+import {createMockChildren} from "../../mockups/child-mock-factory.js";
+import {ElementMock} from "../../mockups/element-mock.js";
+import {EventMock} from "../../mockups/event-mock.js";
+
+await init();
+
+beforeAll(async () => {
+    await import("../../../components/label-counter/label-counter.js");
+})
+
+describe ("label-counter", async () => {
+    let instance;
+    beforeEach(async () => {
+        instance = document.createElement("label-counter");
+        await instance.connectedCallback(() =>{
+            createMockChildren(instance)
+        });
+    })
+
+    it ("init", async () => {
+        assertEquals(instance.value, 1);
+    })
+
+    it ("change", async() => {
+        assertEquals(instance.value, 1);
+        const plusMock = new ElementMock("svg");
+        const minusMock = new ElementMock("svg");
+
+        plusMock.dataset.action = "increment";
+        minusMock.dataset.action = "decrement";
+
+        await instance.clickedHandler(new EventMock(plusMock));
+        assertEquals(instance.value, 2)
+    })
+})
