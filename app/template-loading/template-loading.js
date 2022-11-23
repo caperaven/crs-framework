5a98e453bc68580b4d1427447735e2c147cb0ac8
caperaven <caperaven@gmail.com>
2022-11-23 10:49:51 +0200

    buttons documentation

--- app/template-loading/template-loading.js
+++ app/template-loading/template-loading.js
@@ -1,6 +1,3 @@
-import "/src/binding-extensions/markdown-template.js";
-import "/src/binding-extensions/schema-template.js";
-
 export default class TemplateLoading extends crsbinding.classes.ViewBase {
     async connectedCallback() {
         await super.connectedCallback();
