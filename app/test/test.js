164534e89a6570af39d6215c5759a275d40249dc
Ashton Angel <ashton.angel@pragmaworld.net>
2022-12-13 10:54:43 +0200

    for andre

---
+++ app/test/test.js
@@ -0,0 +1,29 @@
+export default class Test extends crsbinding.classes.ViewBase {
+    async connectedCallback() {
+        await super.connectedCallback();
+        await this.groupData();
+    }
+
+    async disconnectedCallback() {
+        await super.disconnectedCallback();
+    }
+
+    async groupData() {
+        const data = [
+            {title:"title 1", site: "A21", isActive: true},
+            {title:"title 2", site: "A21", isActive: true},
+            {title:"title 3", site: "A11", isActive: true},
+            {title:"title 4", site: "A11", isActive: false},
+            {title:"title 5", site: "A11", isActive: false},
+            {title:"title 6", site: "A11", isActive: true},
+            {title:"title 7", site: "A31", isActive: false}
+        ]
+
+        const result = await crs.call("data", "group", {
+            source: data,
+            fields: ["site", "isActive"]
+        });
+
+        this.json.innerText = JSON.stringify(result);
+    }
+}
