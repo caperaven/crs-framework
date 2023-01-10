6b89d77ab0679688dce24305a0db467c799d7e6e
6Botter7 <7williamsandre7@gmail.com>
2023-01-10 11:06:34 +0200

    Added Templates and slots
    Added Templates and slots and used attributes instead of properties

--- components/designer-repo/designer-repo.js
+++ components/designer-repo/designer-repo.js
@@ -1,16 +1,31 @@
 import "./../filter-header/filter-header.js";
 
 export class DesignerRepo extends crsbinding.classes.BindableElement{
-    static get observedAttribute() {return ["data-repo"]};
+    container = this.shadowRoot.querySelector("#ul-slot")
+
+    static get observedAttributes() {return ["data-repo", "displayStatus"]};
 
     get html() {
         return import.meta.url.replace(".js", ".html");
+    }
+
+    set displayStatus(newValue){
+        this.setProperty("displayStatus", newValue)
+        this.#loadHTML();
+    }
+
+    get displayStatus(){
+        return this.getProperty("displayStatus")
     }
 
     // constructor() {
     //     super();
     //     this.attachShadow({mode:"open"})
     // }
+
+    get shadowDom() {
+        return true
+    }
 
     async connectedCallback() {
         await super.connectedCallback();
@@ -19,6 +34,11 @@ export class DesignerRepo extends crsbinding.classes.BindableElement{
             resize_query : ".resize",
             options: {}
         })
+
+        // pull template from html
+        // clone content for new filter-header
+        // attach the new content to designer-repo component
+        await this.#initTemplates()
     }
 
     async disconnectedCallback () {
@@ -28,19 +48,41 @@ export class DesignerRepo extends crsbinding.classes.BindableElement{
         await super.disconnectedCallback();
     }
 
+    async #initTemplates(){
+        const templateFilter = this.shadowRoot.querySelector("#filter-header"); // had to add the .content
+        const cloneFilter = templateFilter.content.cloneNode(true);
+        // const slot = this.shadowRoot.querySelector('#filter-slot');
+        this.appendChild(cloneFilter);
+        // this.shadowRoot.appendChild(slot)
+
+        const templateUl = this.shadowRoot.querySelector("#ul-template"); // had to add the .content
+        const cloneUl = templateUl.content.cloneNode(true);
+        const slot = this.shadowRoot.querySelector('#main-ul');
+
+        this.appendChild(cloneUl);
+
+    }
+
     async #loadHTML () {
-        const status = this.getProperty("displayStatus");
+        console.log("test");
+        console.log(this.container, "here is container")
+
+
+
+        // const status = this.getProperty("displayStatus");
+        const status = this.displayStatus
 
         if(status == null || this.dataset.repo == null) {
             return ;
         }
         const file = `/templates/designer-repo/${this.dataset.repo}-${status}.html`;
 
-        this.container.innerHTML = await fetch(file).then(result => result.text());
+        this.shadowRoot.querySelector("#ul-slot").innerHTML = await fetch(file).then(result => result.text());
+        // this.container.setAttribute("displayStatus", status)
         const filter = this.getProperty("filter");
         if((filter || "").length > 0 ) {
             for(const child of this.container.children){
-                if(child.dataset.tags.indexOf(filter) == -1) {
+                if(child.dataset.tags.indexOf(filter) === -1) {
                     child.setAttribute("hidden", "hidden");
                 }
             }
@@ -48,7 +90,8 @@ export class DesignerRepo extends crsbinding.classes.BindableElement{
     }
 
     preLoad () {
-        this.setProperty ("displayStatus", "grid");
+        // this.setProperty("displayStatus", "grid");
+        this.displayStatus = "grid"
     }
 
     async displayStatusChanged(newValue) {
