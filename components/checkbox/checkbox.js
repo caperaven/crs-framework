e87330eb67a95319567f620cb8570badf27f40d9
6Botter7 <7williamsandre7@gmail.com>
2023-02-03 10:37:47 +0200

    Added Checkbox Component

---
+++ components/checkbox/checkbox.js
@@ -0,0 +1,116 @@
+/**
+ * @class Checkbox - A custom tristate checkbox element.
+ *
+ * on clicking the component, the state will cycle through "checked", "unchecked", and "null".
+ *
+ * state order :
+ * 1. checked
+ * 2. unchecked
+ * 3. null
+ *
+ * NOTE: data-state and data-title are read-only properties for testability
+ *
+ * @example <caption>Example usage of the checkbox component.</caption>
+ * // this only allows checked and unchecked states
+ * <check-box checked.bind="model.checked" data-state="checked" data-title="Checkbox"></check-box>
+ *
+ * @example <caption>Example usage of the checkbox component as tristate.</caption>
+ * // to allow null state, set data-nullable="true"
+ * <check-box checked.bind="model.checked" data-state="checked" data-title="Checkbox" data-nullable="true"></check-box>
+ */
+export class Checkbox extends HTMLElement {
+    #stateText = {
+        true: "check-box",
+        false: "check-box-blank",
+        mixed: "check-box-mixed"
+    }
+
+    #checked;
+    #iconElement;
+    #titleElement;
+    #clickHandler = this.#clicked.bind(this);
+
+    get html() {
+        return import.meta.url.replace(".js", ".html");
+    }
+
+    get checked() {
+        return this.#checked;
+    }
+
+    set checked(newValue) {
+        this.#checked = newValue;
+        this.#setState(newValue);
+    }
+
+
+    constructor() {
+        super();
+        this.attachShadow({mode: "open"});
+    }
+
+    async connectedCallback() {
+        this.shadowRoot.innerHTML = await fetch(this.html).then(result => result.text());
+        await this.load();
+
+
+
+    }
+
+    async load() {
+        requestAnimationFrame(async () => {
+            this.shadowRoot.addEventListener("click", this.#clickHandler);
+            this.#iconElement = this.shadowRoot.querySelector("#btnCheck");
+            this.#titleElement = this.shadowRoot.querySelector("#lblText");
+
+            this.#setState(this.getAttribute("aria-checked"))
+            this.#titleElement.innerText = this.getAttribute("aria-label");
+        })
+    }
+
+    async disconnectedCallback() {
+        this.shadowRoot.removeEventListener("click", this.#clickHandler);
+        this.#clickHandler = null;
+    }
+
+    /**
+     * It sets the state of the checkbox to the value passed in, and updates the icon and aria-checked attributes
+     * accordingly
+     * @param value - The value to set the checkbox to.
+     */
+    #setState (value) {
+        const nullable = this.dataset.nullable == "true";
+        const state = {
+            true: "true",
+            false: "false",
+            mixed: nullable ? "mixed" : "false"
+        }
+
+        this.setAttribute("aria-checked", state[value]);
+        this.setAttribute("data-state", state[value]);
+        this.#iconElement.innerText = this.#stateText[state[value]];
+        this.#iconElement.setAttribute("aria-checked", state[value]);
+
+    }
+
+    /**
+     * @method clicked - Handles the click event.
+     * Get the next state and use setState to set the state.
+     *
+     * It toggles the state of the checkbox
+     * @param event - The event object that was triggered.
+     */
+    #clicked (event) {
+        const nullable = this.dataset.nullable == "true";
+        const state = this.getAttribute("aria-checked");
+        const nextState = {
+            true: "false",
+            false: nullable ? "mixed" : "true",
+            mixed: "true"
+        }[state];
+        this.#setState(nextState);
+    }
+
+}
+
+customElements.define("check-box", Checkbox);
---
+++ test/components/checbox/checkbox.test.js
@@ -0,0 +1,99 @@
+import { beforeAll, beforeEach, describe, it} from "https://deno.land/std@0.157.0/testing/bdd.ts";
+import { assertEquals } from "https://deno.land/std@0.149.0/testing/asserts.ts";
+import {init} from "./../../../test/mockups/init.js";
+import {createMockChildren} from "../../mockups/child-mock-factory.js";
+import {ElementMock} from "../../mockups/element-mock.js";
+import {EventMock} from "../../mockups/event-mock.js";
+
+let checkboxModule;
+
+beforeAll(async () => {
+    await init();
+    checkboxModule = await import("../../../components/checkbox/checkbox.js");
+});
+
+describe("check-box", () => {
+    let checkbox;
+
+    beforeEach(async () => {
+        checkbox = new checkboxModule.Checkbox();
+        checkbox.shadowRoot = new ElementMock("shadow-root");
+        checkbox.shadowRoot.queryResults["#btnCheck"] = new ElementMock("button", "#btnCheck");
+        checkbox.shadowRoot.queryResults["#lblText"] = new ElementMock("div", "#lblText");
+        checkbox.setAttribute("aria-label", "my-checkbox");
+        checkbox.setAttribute("aria-checked", "true");
+    });
+
+    it("initial state for normal checkbox", () => {
+        assertEquals(checkbox.getAttribute("aria-checked"), "true");
+    });
+
+    it("change state for normal checkbox on click", async () => {
+        assertEquals(checkbox.getAttribute("aria-checked"), "true");
+        checkbox.load();
+        checkbox.shadowRoot.dispatchEvent("click");
+
+        assertEquals(checkbox.getAttribute("aria-checked"), "false");
+    });
+});
+
+
+// let checkboxModule;
+// beforeAll(async () => {
+//     await init();
+//     checkboxModule = await import("../../../components/checkbox/checkbox.js");
+// });
+//
+// describe("check-box", () => {
+//     let checkbox;
+//
+//     beforeEach(async () => {
+//         checkbox = new checkboxModule.Checkbox();
+//         document.body.appendChild(checkbox);
+//         checkbox.setAttribute("aria-label", "my-checkbox");
+//         checkbox.setAttribute("aria-checked", true);
+//         await checkbox.updateComplete;
+//     });
+//
+//     it("initial state for normal checkbox", () => {
+//         checkbox.isTristate = false;
+//         checkbox.setAttribute("aria-checked", true);
+//         assertEquals(checkbox.getAttribute("aria-checked"), true);
+//     });
+//
+//     it("change state for normal checkbox on click", () => {
+//         assertEquals(checkbox.getAttribute("aria-checked"), true);
+//         checkbox.click();
+//         assertEquals(checkbox.getAttribute("aria-checked"), false);
+//     });
+// });
+
+// let checkboxModule;
+// beforeAll(async () => {
+//     await init();
+//     checkboxModule = await import("../../../components/checkbox/checkbox.js");
+// });
+//
+// describe("check-box", () => {
+//     let checkbox;
+//
+//     beforeEach(async () => {
+//         checkbox = new checkboxModule.Checkbox();
+//         document.body.appendChild(checkbox);
+//         checkbox.setAttribute("aria-label", "my-checkbox");
+//         checkbox.setAttribute("aria-checked", "true");
+//         await checkbox.updateComplete;
+//     });
+//
+//     it("initial state for normal checkbox", () => {
+//         checkbox.setAttribute("aria-checked", true);
+//         assertEquals(checkbox.getAttribute("aria-checked") === "true", true);
+//     });
+//
+//     it("change state for normal checkbox on click", async () => {
+//         assertEquals(checkbox.getAttribute("aria-checked") === "true", true);
+//         const button = checkbox.shadowRoot.querySelector("button[role='checkbox']");
+//         button.dispatchEvent(new Event("click"));
+//         assertEquals(checkbox.getAttribute("aria-checked") === "false", false);
+//     });
+// });
