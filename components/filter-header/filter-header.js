9efec354b3dcc2bc77c23a60c738ee6d750b1485
6Botter7 <7williamsandre7@gmail.com>
2023-02-15 16:50:15 +0200

    Refactor and Start Test-Cases
    Refactored HTML adding slot , using ICON class
    Refactored JS, adding load FN, with requestedAnimationFrame.
    Started Deno Test

--- components/filter-header/filter-header.js
+++ components/filter-header/filter-header.js
@@ -1,3 +1,11 @@
+/**
+ * @class FilterHeader - It's a custom element that filters the children of a container element.
+ *
+ * Features:
+ * filter - filters the children of the container element
+ * close - closes the filter
+ *
+ */
 class FilterHeader extends crsbinding.classes.BindableElement {
     #container;
 
@@ -9,17 +17,53 @@ class FilterHeader extends crsbinding.classes.BindableElement {
         return import.meta.url.replace(".js", ".html");
     }
 
-    async connectedCallback() {
-        super.connectedCallback();
-        const query = this.getAttribute("for");
-        this.#container = this.parentElement.querySelector(query);
+    constructor() {
+        super();
+        // this.attachShadow({ mode: "open" });
     }
 
+    /**
+     * @method connectedCallback - When the component is added to the DOM, the container element is set to the element
+     * that has the id that is specified in the "for" attribute.
+     */
+    async connectedCallback() {
+        super.connectedCallback();
+        // const query = this.getAttribute("for");
+        // this.#container = this.parentElement.querySelector(query);
+        // this.shadowRoot.innerHTML = await fetch(this.html).then(result => result.text());
+        await this.load();
+
+    }
+
+    load() {
+        return new Promise(resolve => {
+            requestAnimationFrame(async () => {
+                const query = this.getAttribute("for");
+                this.#container = this.parentElement.querySelector(query);
+                resolve();
+            });
+        });
+    }
+
+    /**
+     * @method disconnectedCallback - When the component is removed from the DOM, the container is set to null
+     */
     async disconnectedCallback() {
         this.#container = null;
         await super.disconnectedCallback();
     }
 
+    /**
+     * @method filter - "If the user presses the down arrow key, then dispatch a focus-out event. Otherwise, filter the children of the
+     * container element based on the value of the input element."
+     *
+     * The first line of the function is an if statement. If the event's code is "ArrowDown", then the function will return
+     * the result of the dispatchEvent function. The dispatchEvent function is a built-in JavaScript function that
+     * dispatches an event. The event that is dispatched is a CustomEvent. A CustomEvent is a type of event that can be
+     * created by the user. The CustomEvent is named "focus-out"
+     * @param event - The event object that was passed to the event listener.
+     * @returns the result of the dispatchEvent function.
+     */
     async filter(event) {
         if (event.code == "ArrowDown") {
             return this.dispatchEvent(new CustomEvent("focus-out"));
@@ -31,6 +75,9 @@ class FilterHeader extends crsbinding.classes.BindableElement {
         })
     }
 
+    /**
+     * The function `close()` dispatches a custom event called `close` to the DOM
+     */
     async close() {
         this.dispatchEvent(new CustomEvent("close"));
     }
---
+++ test/components/filter-header/filter-header.test.js
@@ -0,0 +1,50 @@
+import { beforeAll, beforeEach, afterEach, describe, it} from "https://deno.land/std@0.157.0/testing/bdd.ts";
+import { assertEquals, assert } from "https://deno.land/std@0.149.0/testing/asserts.ts";
+import {init} from "./../../../test/mockups/init.js";
+import {createMockChildren, ElementMock} from "../../mockups/element-mock.js";
+import {EventMock} from "../../mockups/event-mock.js";
+
+
+await init();
+
+beforeAll(async () => {
+    await import("../../../components/filter-header/filter-header.js");
+})
+
+describe ("filter-header tests", async () => {
+    let instance;
+    let parent;
+    let ul;
+
+    beforeEach(async () => {
+        parent = new ElementMock("div");
+        parent.id = "parent";
+        ul = new ElementMock("ul");
+        parent.appendChild(ul);
+
+        instance = document.createElement("filter-header");
+        instance.setAttribute("for", "ul");
+        parent.appendChild(instance);
+
+
+
+        await instance.connectedCallback();
+
+        // instance = document.createElement("filter-header");
+        // instance.setAttribute("for", "ul");
+        // parent =  new ElementMock("div");
+        // instance.parentElement = parent;
+        // parent.appendChild(instance);
+        // await instance.connectedCallback();
+    });
+
+    afterEach(async () => {
+        await instance.disconnectedCallback();
+    });
+
+    it("parent container mock", async () => {
+        assert(instance !== null);
+    });
+
+
+});
