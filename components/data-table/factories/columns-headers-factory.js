8fed01ba2985d550495b736d0fff814cb7e17838
caperaven <caperaven@gmail.com>
2023-03-06 12:08:01 +0200

    add columns manager to data table

---
+++ components/data-table/factories/columns-headers-factory.js
@@ -0,0 +1,22 @@
+/**
+ * @function columnsHeadersFactory - create column header cells from the columns definitions.
+ * The columns definitions are the columns on the columns manager and contains the following properties:
+ * - title: the title of the column
+ * - width: the width of the column
+ * - property: the property name of the column
+ * @param columns
+ * @returns {Promise<DocumentFragment>}
+ */
+export async function columnsHeadersFactory(columns) {
+    let fragment = document.createDocumentFragment();
+
+    for (const column of columns) {
+        let th = document.createElement("div");
+        th.role = "columnheader";
+        th.dataset.field = column.property;
+        th.textContent = column.title;
+        fragment.appendChild(th);
+    }
+
+    return fragment;
+}
