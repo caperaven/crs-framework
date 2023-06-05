25816b02d17d4e4603ee05900e2669d5277aa81d
Charles Lemmert <81480730+Astro-Alien@users.noreply.github.com>
2023-06-05 16:06:20 +0200

    Feature/cml/layout component (#50)
    * committing layout component
    
    * committing first tests
    
    * committing name change
    
    * Update layout-component.test.js
    
    * Update layout.html
    
    * Update layout.html
    
    * committing changes
    
    Converted component to HTMLElement instead of bindable element removed the .html of the component and removed it from the shadowDom and deleted the <slot>
    
    * removing load function

---
+++ app/layout/layout.js
@@ -0,0 +1,12 @@
+import "../../components/layout-container/layout-container.js";
+
+export default class Layout extends crsbinding.classes.ViewBase {
+
+    async connectedCallback() {
+        await super.connectedCallback();
+    }
+
+    async disconnectedCallback() {
+        await super.disconnectedCallback();
+    }
+}
