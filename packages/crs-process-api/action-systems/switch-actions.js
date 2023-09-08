b8f3da34eba932ac0e3bfab09f1f022a6c9804bd
Gerhard Meyer <gerhard.meyer@pragmaworld.net>
2023-09-08 15:46:22 +0200

    Upgrade process api

--- packages/crs-process-api/action-systems/switch-actions.js
+++ packages/crs-process-api/action-systems/switch-actions.js
@@ -1,18 +1,1 @@
-class SwitchActions {
-  static async perform(step, context, process, item) {
-    const value = await crs.process.getValue(step.args.check, context, process, item);
-    const cases = await crs.process.getValue(step.args.cases, context, process, item);
-    const defaultStep = await crs.process.getValue(step.args.default, context, process, item);
-    const nextStepKey = cases[value] || defaultStep;
-    if (nextStepKey == null)
-      return;
-    const nextStep = await crs.getNextStep(process, nextStepKey);
-    if (nextStep != null) {
-      await crs.process.runStep(nextStep, context, process, item);
-    }
-  }
-}
-crs.intent.switch = SwitchActions;
-export {
-  SwitchActions
-};
+class i{static async perform(c,s,a,t){const n=await crs.process.getValue(c.args.check,s,a,t),l=await crs.process.getValue(c.args.cases,s,a,t),u=await crs.process.getValue(c.args.default,s,a,t),e=l[n]||u;if(e==null)return;const r=await crs.getNextStep(a,e);r!=null&&await crs.process.runStep(r,s,a,t)}}crs.intent.switch=i;export{i as SwitchActions};
