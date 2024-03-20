575b5e4cf5905d5b042758ececf05e3ce2dadaba
Andre Williams <7williamsandre7@gmail.com>
2024-03-20 13:26:44 +0200

    Added files needed for sla-visualization

---
+++ components/sla-visualization/sla-visualization.js
@@ -0,0 +1,10 @@
+// /**
+//  * @class SlaVisualization - This class is responsible for rendering the SLA visualization.
+//  * @extends crs.classes.BindableElement
+//  *
+//  *
+//  **/
+//
+// export class SlaVisualization extends crs.classes.BindableElement {
+//
+// }
