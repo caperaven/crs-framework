7bf20f4ade98fd09f068b765e4392c679b64ffc9
caperaven <caperaven@gmail.com>
2024-09-03 15:41:00 +0200

    add initial matrix

---
+++ components/matrix-renderer/align.js
@@ -0,0 +1,5 @@
+export const Align = Object.freeze({
+    LEFT: 1,
+    RIGHT: 2,
+    CENTER: 3,
+});
---
+++ components/matrix-renderer/data-type.js
@@ -0,0 +1,9 @@
+export const DataType = Object.freeze({
+    TEXT: 1,
+    BOOLEAN: 2,
+    NUMBER: 3,
+    DATE: 4,
+    TIME: 5,
+    DURATION: 6,
+    GEO: 7
+});
