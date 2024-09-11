7424a8790655d4c8e41abc184b6c6bd8cb82888d
caperaven <caperaven@gmail.com>
2024-09-11 15:36:11 +0200

    matrix - cell click

--- components/matrix-renderer/editors/editor.js
+++ components/matrix-renderer/editors/editor.js
@@ -16,10 +16,5 @@ export function createEditorLT() {
 }
 
 export function todo(ctx, def, rowIndex, column, aabb) {
-    // console.log(aabb)
-    //
-    // ctx.save()
-    // ctx.fillStyle = "blue";
-    // ctx.fillRect(aabb.x1, aabb.y1, aabb.x2 - aabb.x1, aabb.y2 - aabb.y1);
-    // ctx.restore()
+    return;
 }
