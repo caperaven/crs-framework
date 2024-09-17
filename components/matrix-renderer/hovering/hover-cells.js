c5ec7745fbe816eeb42ead353f1f294c7ceaf533
caperaven <caperaven@gmail.com>
2024-09-17 15:34:42 +0200

    matrix - hover manager

--- components/matrix-renderer/hovering/hover-cells.js
+++ components/matrix-renderer/hovering/hover-cells.js
@@ -1,3 +1,16 @@
 export function hoverCells(ctx, parentElement, details) {
-    return console.log("hover on cell")
+    const isInFrozenZone = details.offsetX < (details.def.regions.frozenColumns?.right ?? 0);
+
+    const y = details.scrollTop + details.offsetY - details.def.regions.cells.top;
+    const x = isInFrozenZone ? details.offsetX : details.offsetX + details.scrollLeft;
+
+    const rowIndex = details.rowSizes.getIndex(y);
+    const columnIndex = details.columnSizes.getIndex(x);
+
+    if (details.def.errors != null) {
+        const error = details.def.errors[`${rowIndex},${columnIndex}`];
+        if (error != null) {
+            console.log(`hovered on error: ${error.message}`);
+        }
+    }
 }
