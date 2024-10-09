d7c64c7e082c15356551d4a119603ef81ca7af41
Charles Lemmert <81480730+Astro-Alien@users.noreply.github.com>
2024-10-09 16:04:39 +0200

    committing updated tablist test

--- test/components/tab-list/tab-list.test.js
+++ test/components/tab-list/tab-list.test.js
@@ -11,44 +11,88 @@ beforeAll(async () => {
 })
 
 describe ("tab-list", async () => {
-    let instance;
-    let perspective;
+    let instance,listContainer;
 
     beforeEach(async () => {
-        perspective = document.createElement("perspective-element");
-        perspective.id = "target";
-        document.appendChild(perspective);
+        const listContainer = document.createElement("div");
+        listContainer.id = "target";
 
-        createChildrenFromHtml(perspective, [
-            '<template data-id="view1" data-default="true"><h1>Template 1</h1></template>',
-            '<template data-id="view2"><h1>Template 2</h1></template>',
-            '<template data-id="view3"><h1>Template 3</h1></template>'
-        ].join(" "), false)
+        document.appendChild(listContainer);
 
-        perspective.connectedCallback();
+        createChildrenFromHtml(listContainer, [
+            '<ul data-id="view1" id="view-one"><li>View 1</li></ul>',
+            '<ul data-id="view2" id="view-two"><li>View 2</li></ul>',
+            '<ul data-id="view3" id="view-three"><li>View 3</li></ul>'
+        ].join(" "), false);
+
 
         instance = document.createElement("tab-list");
         instance.setAttribute("for", "#target");
-
-        await instance.connectedCallback();
 
         createChildrenFromHtml(instance, [
             '<tab data-view="view1" aria-selected="true">&{tab.view1}</tab>',
             '<tab data-view="view2">&{tab.view2}</tab>',
             '<tab data-view="view3">&{tab.view3}</tab>'
-        ].join(" "))
+        ].join(" "));
+
+        document.appendChild(instance);
     })
 
-    it("change", async () => {
+    it("tablist - select the first list item", async () => {
+        const listItemContainer = await setInitialMockFunctions(document, instance, 0);
+        await instance.connectedCallback();
+
         const selected = instance.shadowRoot.querySelectorAll("[aria-selected='true']");
+
         assertEquals(selected.length, 1);
         assertEquals(selected[0].dataset.view, "view1");
-        assertEquals(perspective.children[0].textContent, "Template 1");
+        assertEquals(listItemContainer.children[0].hidden, false);
 
-        await instance.performEvent("click", instance.shadowRoot.children[3]);
-        const selected2 = instance.shadowRoot.querySelectorAll("[aria-selected='true']");
-        assertEquals(selected2.length, 1);
-        assertEquals(selected2[0].dataset.view, "view3");
-        assertEquals(perspective.children[0].textContent, "Template 3");
-    })
-})
+        assertEquals(listItemContainer.children[1].hidden, true);
+        assertEquals(listItemContainer.children[2].hidden, true);
+    });
+
+    it("tablist - select the second list item", async () => {
+        const listItemContainer = await setInitialMockFunctions(document, instance, 1);
+        await instance.connectedCallback();
+
+        await instance.performEvent("click", instance.shadowRoot.children[1]);
+
+        const selected = instance.shadowRoot.querySelectorAll("[aria-selected='true']");
+        assertEquals(selected.length, 1);
+        assertEquals(selected[0].dataset.view, "view2");
+        assertEquals(listItemContainer.children[1].hidden, false);
+
+        assertEquals(listItemContainer.children[0].hidden, true);
+        assertEquals(listItemContainer.children[2].hidden, true);
+    });
+
+    it("tablist - select the third list item", async () => {
+        const listItemContainer = await setInitialMockFunctions(document, instance, 2);
+        await instance.connectedCallback();
+
+        await instance.performEvent("click", instance.shadowRoot.children[2]);
+
+        const selected = instance.shadowRoot.querySelectorAll("[aria-selected='true']");
+        assertEquals(selected.length, 1);
+        assertEquals(selected[0].dataset.view, "view3");
+        assertEquals(listItemContainer.children[2].hidden, false);
+
+        assertEquals(listItemContainer.children[0].hidden, true);
+        assertEquals(listItemContainer.children[1].hidden, true);
+    });
+});
+
+async function setInitialMockFunctions(document, instance, itemIndex) {
+    const listItemContainer = document.body.children[0]
+    instance.target = listItemContainer;
+    instance.querySelector = () => {
+        return instance.shadowRoot.children[itemIndex];
+    }
+
+    listItemContainer.querySelectorAll = () => {
+        return listItemContainer.children;
+    }
+
+    return listItemContainer;
+}
