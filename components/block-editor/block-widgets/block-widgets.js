7119a1b6d53edba5ce68fb16341e95d607ec3d12
caperaven <caperaven@gmail.com>
2024-10-30 12:27:21 +0200

    block editing - block widgets component

--- components/block-editor/block-widgets/block-widgets.js
+++ components/block-editor/block-widgets/block-widgets.js
@@ -6,6 +6,30 @@ export default class BlockWidgets extends crsbinding.classes.BindableElement {
     get html() {
         return import.meta.url.replace(".js", ".html")
     }
+
+    async load() {
+        const data = await crsbinding.events.emitter.emit("getWidgetLibrary");
+        const groups = Object.groupBy(data.widgets, ({ category }) => category);
+        const template = this.shadowRoot.querySelector("#widget-section");
+        const container = this.shadowRoot.querySelector("#widgets-container");
+
+        for (const [key, value] of Object.entries(groups)) {
+            const instance = template.content.cloneNode(true);
+            instance.querySelector("[role='group']").ariaLabel = key;
+            instance.querySelector("h2").textContent = key;
+            const ulElement = instance.querySelector("ul");
+
+            for (const item of value) {
+                const liElement = document.createElement("li");
+                liElement.textContent = item.label;
+                liElement.dataset.icon = item.icon;
+                liElement.dataset.keywords = item.keywords.join(" ");
+                ulElement.appendChild(liElement);
+            }
+
+            container.appendChild(instance);
+        }
+    }
 }
 
 customElements.define("block-widgets", BlockWidgets);
